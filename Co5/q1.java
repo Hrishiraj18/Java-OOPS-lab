9d3848f9310e65b0e544affb761babff5fa72c80
Hrishiraj18 <121205156+Hrishiraj18@users.noreply.github.com>
2023-06-26 05:59:17 +0530

    CO5 Q1

---
+++ Co5/q1.java
@@ -0,0 +1,15 @@
+package javax;
+import java.awt.*;
+import java.applet.*;
+
+public class q1 extends Applet{
+
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		g.setColor(Color.BLUE);
+		g.drawLine(180,10,180,100);
+		g.drawOval(20,20,100,100);
+		g.drawRect(20,140,100,100);
+	}
+
+}
